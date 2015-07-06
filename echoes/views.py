201b144f97730c2e2165c2b4cbebcf947a842ad3
Mary Connolly <mconnol6@nd.edu>
2015-07-05 21:50:02 -0400

    added performances page

--- echoes/views.py
+++ echoes/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
+def performances(request):
+	return render(request, 'echoes/performances.html')
+
 def homepage(request):
 	return render(request, 'echoes/homepage.html')
