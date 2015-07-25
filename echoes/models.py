64151935bcff753c6d8e14eb12ea42eb6f5711e9
Mary Connolly <mconnol6@nd.edu>
2015-07-25 14:29:19 -0400

    changed Performance model and added superuser

--- echoes/models.py
+++ echoes/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from datetime import datetime
+from django.utils import timezone
 
 class Performance(models.Model):
 	title=models.CharField('Title', default='', max_length=200)
 	location=models.CharField('Where', default='', max_length=200)
-	date=models.DateField('Date', default=datetime.now())
-	meetingTime=models.TimeField('Meeting Time', default=datetime.now())
-	startTime=models.TimeField('Start Time', default=datetime.now())
-	endTime=models.TimeField('End Time', default=datetime.now())
+	date=models.DateField('Date', default=timezone.now)
+	meetingTime=models.TimeField('Meeting Time', default=timezone.now)
+	startTime=models.TimeField('Start Time', default=timezone.now)
+	endTime=models.TimeField('End Time', default=timezone.now)
 	details=models.TextField('Details', default='')
