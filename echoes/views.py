aea5236266ab57f44ff14bd8c1ec19104e4050de
Mary Connolly <mconnol6@nd.edu>
2015-07-25 12:50:02 -0400

    New header with animated gif

--- echoes/views.py
+++ echoes/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
+from .models import Performance
+
 def performances(request):
-	return render(request, 'echoes/performances.html')
+	performances = Performance.objects.all()
+	context = {'performances': performances }
+	return render(request, 'echoes/performances.html', context)
 
 def homepage(request):
 	return render(request, 'echoes/homepage.html')
